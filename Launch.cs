d222b33f11603940853c55117904c00ff6b999de
Rien Sach <rien.sach@btinternet.com>
2023-12-16 12:27:59 +0000

    day 15

--- Launch.cs
+++ Launch.cs
@@ -16,7 +16,7 @@ namespace AdventofCode2023
             Console.WriteLine("------------------------\n");
 
 
-            int daySelection = 10;
+            int daySelection = 15;
             int yearSelection = 2023;
             int partSelection = 1;
 
