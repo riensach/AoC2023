b1a5dbe13f0b33d761eb77e89578232e7bc1d10b
Rien Sach <rien.sach@btinternet.com>
2023-12-19 18:18:28 +0000

    Day 13 part b!

--- solution/2023/AoC2023Day13.cs
+++ solution/2023/AoC2023Day13.cs
@@ -14,30 +14,22 @@ namespace AoC2023.solution
         public class AoCDay13
         {
         public string[,] grid;
+        public string[,] gridTemp;
         int arrayLength = 0;
         int arrayWidth = 0;
 
         public AoCDay13(int selectedPart, string input)
         {
-            string[] grids = input.Split("\n\r\n");
-
-            Console.WriteLine(grids[0]);
-            Console.WriteLine("space");
-            Console.WriteLine(grids[1]);
-
-            
+            string[] grids = input.Split("\n\r\n");            
             int horizontalReflectionRowsAbove = 0;
-            int horizontalReflectionRowsAboveTemp = 0;
+            int horizontalReflectionRowsAboveSecond = 0;
             int verticalReflectionColumnsLeft = 0;
-            int verticalReflectionColumnsLeftTemp = 0;
+            int verticalReflectionColumnsLeftSecond = 0;
             foreach (string data in grids)
             {
                 string[] lines = data.Split("\n");
                 lines[0] = lines[0].Replace("\r", "").Replace("\n", "");
 
-
-                verticalReflectionColumnsLeftTemp = 0;
-                horizontalReflectionRowsAboveTemp = 0;
                 arrayLength = lines.Count();
                 arrayWidth = lines[0].Length;
 
@@ -175,6 +167,268 @@ namespace AoC2023.solution
 
             int finalCalt = verticalReflectionColumnsLeft + (horizontalReflectionRowsAbove * 100);
             output = "Part A: " + finalCalt;
+
+
+            foreach (string data in grids)
+            {
+                string[] lines = data.Split("\n");
+                lines[0] = lines[0].Replace("\r", "").Replace("\n", "");
+
+                arrayLength = lines.Count();
+                arrayWidth = lines[0].Length;
+
+                grid = new string[arrayLength, arrayWidth];
+                gridTemp = new string[arrayLength, arrayWidth];
+                grid = createGrid(grid, arrayLength, arrayWidth);
+                gridTemp = createGrid(gridTemp, arrayLength, arrayWidth);
+                int row = 0;
+                int column = 0;
+                foreach (string line in lines)
+                {
+                    line.Replace("\r", "").Replace("\n", "");
+                    foreach (var character in line)
+                    {
+                        if (character.ToString() == "\r") continue;
+                        if (character.ToString() == "\n") continue;
+                        //Console.WriteLine(character.ToString());
+                        grid[row, column] = character.ToString();
+                        gridTemp[row, column] = character.ToString();
+                        column++;
+                    }
+                    row++;
+                    column = 0;
+                }
+
+                bool isReflection = false;
+                for (int a = 0; a < arrayLength; a++)
+                {
+                    if (isReflection == true)
+                    {
+                        Console.WriteLine("wut1"); 
+                        break;
+                    }
+                    for (int b = 0; b < arrayWidth; b++)
+                    {
+                        if (isReflection == true)
+                        {
+                            Console.WriteLine("wut");
+                            break;
+                        }
+                        //
+                        //gridTemp[x, y] = ".";
+
+                        
+                        for (int x = arrayLength - 1; x >= -1; x--)
+                        {
+                            isReflection = false;
+                            int z = 1;
+                            int reflectionRows = 0;
+                            while ((x - z + 1 > -1) && (x + z < arrayLength))
+                            {
+
+                                int lineBefore = (x - z) + 1;
+                                int lineAfter = x + z;
+                                string lineComp1 = "";
+                                string lineComp2 = "";
+                                for (int w = 0; w < arrayWidth; w++)
+                                {
+                                    if(a==lineBefore && b == w)
+                                    {
+                                        Console.WriteLine("got here3");
+                                        //lineComp1 = lineComp1 + (grid[lineBefore, w] == "." ? "#": ".");
+                                        lineComp1 = lineComp1 + grid[lineBefore, w];
+                                    } else
+                                    {
+                                        lineComp1 = lineComp1 + grid[lineBefore, w];
+                                    }
+                                    
+                                }
+                                for (int w = 0; w < arrayWidth; w++)
+                                {
+                                    if (a == lineAfter && b == w)
+                                    {
+                                        Console.WriteLine("got here4");
+                                        //lineComp2 = lineComp2 + (grid[lineAfter, w] == "." ? "#" : ".");
+                                        lineComp2 = lineComp2 + grid[lineAfter, w];
+                                    }
+                                    else
+                                    {
+                                        lineComp2 = lineComp2 + grid[lineAfter, w];
+                                    }                                    
+                                }
+
+                                //Console.WriteLine("Checking lines "+ lineBefore+" and "+ lineAfter);
+                                //Console.WriteLine(lineComp1);
+                                //Console.WriteLine(lineComp2);
+                                //Console.WriteLine("Checking linesz ");
+                                if (lineComp1 == lineComp2)
+                                {
+                                    //Console.WriteLine("got here"); 
+                                    reflectionRows++;
+                                    isReflection = true;
+
+                                }
+                                else
+                                {
+                                    //Console.WriteLine("got here wut");
+                                    isReflection = false;
+                                    break;
+                                }
+                                z++;
+                            }
+                            //Console.WriteLine(isReflection);
+                            if (isReflection == true)
+                            {
+                                reflectionRows = x + 1;
+                                horizontalReflectionRowsAboveSecond += reflectionRows;
+                                Console.WriteLine("Horizontal reflection at line " + x + " with row count of " + reflectionRows + " with a max rows of " + arrayLength);
+                                break;
+                            }
+
+                        }
+                        if (isReflection == true)
+                        {
+                            //break;
+                        }
+
+                        isReflection = false;
+                        for (int x = arrayWidth - 1; x >= -1; x--)
+                        {
+                            isReflection = false;
+                            int z = 1;
+                            int reflectionRows = 0;
+                            while ((x - z + 1 > -1) && (x + z < arrayWidth))
+                            {
+
+                                // Check for reflection horizontal
+                                if ((x - z + 1 < 0) || (x + z >= arrayWidth))
+                                {
+                                    break;
+                                }
+                                int lineBefore = (x - z) + 1;
+                                int lineAfter = x + z;
+                                string lineComp1 = "";
+                                string lineComp2 = "";
+
+                                for (int w = 0; w < arrayLength; w++)
+                                {
+                                    if (a == w && b == lineBefore)
+                                    {
+                                        Console.WriteLine("got here2");
+                                        //lineComp1 = lineComp1 + (grid[w, lineBefore] == "." ? "#" : ".");
+                                        lineComp1 = lineComp1 + grid[w, lineBefore];
+                                    }
+                                    else
+                                    {
+                                        lineComp1 = lineComp1 + grid[w, lineBefore];
+                                    }
+
+                                }
+                                for (int w = 0; w < arrayLength; w++)
+                                {
+                                    if (a == w && b == lineAfter)
+                                    {
+                                        Console.WriteLine("got here1");
+                                        //lineComp2 = lineComp2 + (grid[w, lineAfter] == "." ? "#" : ".");
+                                        lineComp2 = lineComp2 + grid[w, lineAfter];
+                                    }
+                                    else
+                                    {
+                                        lineComp2 = lineComp2 + grid[w, lineAfter];
+                                    }
+                                }
+
+
+                                //Console.WriteLine("Checking lines " + lineBefore + " and " + lineAfter);
+                                //Console.WriteLine(arrayWidth);
+                                //Console.WriteLine(lineComp1);
+                                //Console.WriteLine(lineComp2);
+                                //Console.WriteLine("Checking linesz ");
+                                if (lineComp1 == lineComp2)
+                                {
+                                    reflectionRows++;
+                                    isReflection = true;
+                                }
+                                else
+                                {
+                                    isReflection = false;
+                                    break;
+                                }
+                                z++;
+                            }
+                            if (isReflection == true)
+                            {
+                                reflectionRows = x + 1;
+                                verticalReflectionColumnsLeftSecond += reflectionRows;
+                                Console.WriteLine("Vertical reflection at line " + x + " with row count of " + reflectionRows + " with a max rows of " + arrayLength);
+                                break;
+                            }
+
+                        }
+                        if (isReflection == true)
+                        {
+                            //break;
+                        }
+                    }
+                }
+                
+
+
+            }
+
+            int finalCaltSecond = verticalReflectionColumnsLeftSecond + (horizontalReflectionRowsAboveSecond * 100);
+            output += "\nPart B: " + finalCaltSecond;
+
+
+
+
+
+            return;
+            /*for (int x = 0; x < xSize; x++)
+            {
+                for (int y = 0; y < ySize; y++)
+                {
+                    grid[x, y] = ".";
+                }
+            }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             //output += "Part B:" + totalLoad;
 
